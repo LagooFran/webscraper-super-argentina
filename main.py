47108d8b388ee044c534b1061425870aa8d54fe2
franpc <fran080j9@gmail.com>
2025-03-26 17:07:17 -0300

    link generator working
    should add coments to code in next commit

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from tools import vtexRequestBuilder
+import requests
 
-print(vtexRequestBuilder.FormatParams())
+res = requests.get(vtexRequestBuilder.BuildRequest("arroz", "https://www.carrefour.com.ar"))
+
+for key in res.json()["data"]["productSuggestions"]["products"]:
+    print(key["productName"])
--- tools/vtexRequestBuilder.py
+++ tools/vtexRequestBuilder.py
@@ -1,9 +1,13 @@
-def BuildRequest(query, site, url):
-    return
+import urllib.parse as urlib
+import base64 as b64
+import json as j
 
+def BuildRequest(query, url):
+    
+    finalRequest = url + "/_v/segment/graphql/v1" + FormatParams()
+    finalRequest += "&variables={}" + "&extensions=" + GetExtensions(query)
 
-def GetExtensions():
-    return
+    return finalRequest
 
 def FormatParams():
     #Si se necesitara un cambio en los parametros modificar aqui.
@@ -29,3 +33,31 @@ def FormatParams():
             formattedParams += f"&{item}={params[item]}"  
 
     return formattedParams
+
+def GetExtensions(query):
+    #json pre armado
+    variables = {
+        "count" : 4,
+        "fullText" : query,
+        "hideUnavailableItems" : True,
+        "productOriginVtex" : True,
+        "shippingOptions" : [],
+        "simulationBehavior" : "default", 
+        "variant" : None
+    }
+
+    variablesJson = j.dumps(variables, separators=(',',':'))
+    variablesEncoded = b64.b64encode(bytes(variablesJson, "utf8"))
+
+    extensions = {"persistedQuery" : {
+        "version" : 1,
+        "sha256Hash" : "0ef2c56d9518b51f912c2305ac4b07851c265b645dcbece6843c568bb91d39ff",
+        "sender" : "vtex.store-resources@0.x",
+        "provider" : "vtex.search-graphql@0.x"
+    }, "variables" : str(variablesEncoded.decode()) }
+
+    
+    extensionsJson = j.dumps(extensions, separators=(',',':'))
+
+
+    return str(extensionsJson)
