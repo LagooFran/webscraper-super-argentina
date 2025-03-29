d5dddf66af59a3bd02fc541ca7fb18703ff9a3d5
franpc <fran080j9@gmail.com>
2025-03-29 12:03:08 -0300

    Added scrapers for most vtex sites

--- scrapers/vtexScrapers.py
+++ scrapers/vtexScrapers.py
@@ -7,3 +7,21 @@ def GetCarrefour(query, cant, debug):
 def GetJumbo(query, cant, debug):
     prodsRes = vtexJsonFinder.GetProds(query, cant, "https://www.jumbo.com.ar", debug)
     return prodsRes
+
+def GetDia(query, cant, debug):
+    prodsRes = vtexJsonFinder.GetProds(query, cant, "https://diaonline.supermercadosdia.com.ar", debug)
+    return prodsRes
+
+def GetDisco(query, cant, debug):
+    prodsRes = vtexJsonFinder.GetProds(query, cant, "https://www.disco.com.ar", debug)
+    return prodsRes
+
+def GetVea(query, cant, debug):
+    prodsRes = vtexJsonFinder.GetProds(query, cant, "https://www.vea.com.ar", debug)
+    return prodsRes
+
+
+def GetMas(query, cant, debug):
+    prodsRes = vtexJsonFinder.GetProds(query, cant, "https://www.masonline.com.ar", debug)
+    return prodsRes
+
